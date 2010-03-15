e438897492b4408a25509947460a2beef9a29fd1
elenz <elenz@f9ec7c9c-190f-40df-3b6b-822edbfbe705>
2010-03-15 18:59:49 +0000

    Updated the CSS and JavaScript (from Ylva). Among other things, added this behavior: upper-right-hand navigation items are automatically expanded when they contain the current page's link.

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,7 @@ if(typeof jQuery != 'undefined') {
 	        "Search documents by keyword"
 	    );
 		}
-		$('#sub > div:last').addClass('last'); // only supposed to add some last-child functionality to IE
+		$('#sub > div:last-child').addClass('last'); // only supposed to add some last-child functionality to IE
 		if(jQuery().tabs) {
 			$('#special_intro .nav').tabs('#special_intro > div',{
 				//effect: 'fade',
@@ -16,27 +16,102 @@ if(typeof jQuery != 'undefined') {
 			});
 		}
 		// accordion style menu
-		$('#sub .subnav h2, #sub .subnav li span').addClass('closed').next().hide();
+		$('#sub .subnav h2, #sub .subnav li span').each(function() {
+			if(!($(this).next().children().is('.current'))) {
+				$(this).addClass('closed').next().hide();
+			}
+		})
+		
+		$('#sub .current').parents().show();
 		$('#sub .subnav h2, #sub .subnav li span').click(function() {
 			$(this).toggleClass('closed').next().toggle('normal');
 		});
-		// signup form overlay
+		// overlay functions
 		if(jQuery().overlay) {
 			$('body').append('<div class="overlay" id="overlay"><div class="overlayContent"></div></div>');
+			//signup overlay
 			$('#utilnav .signup a').attr('rel','#overlay').overlay({
 				expose: 'black',
 				left: 'center',
 				closeOnClick: false,
 				onBeforeLoad: function() { 
-	       if ($.browser.msie) {$('#sub .subnav h2, #sub .subnav li span').unbind();}  //IE needs this otherwise it doubles up the event handler
-	       // grab wrapper element inside content 
 	        var wrap = this.getContent().find(".overlayContent"); 
-	        // load the page specified in the trigger 
-	      	wrap.load(this.getTrigger().attr("href"));
+	      	wrap.load(this.getTrigger().attr("href") + ' .popup');
+	      },
+	      onLoad: function() {
+	   			$('.popup input[type=reset]').replaceWith('<input type="button" class="cancel" value="Cancel" />');
+    			$('.popup input.cancel').click(function() {
+    				$('#utilnav .signup a[rel]').overlay().close();
+    			});
+}
+      });
+      // generic function to prevent form submission for certain submit buttons
+      function preventSubmit(form,trigger) {
+	      $(form).submit(function(e) {
+	        if (e.originalEvent.explicitOriginalTarget.name == trigger || e.originalEvent.explicitOriginalTarget.className == trigger) {
+	          e.preventDefault();
+	          return false;
+	        }
+	    	});
+    	}
+    	// invoke specific submission prevention
+    	preventSubmit('form#codeedit','add_media');
+      // begin add media overlay
+      $('input[name=add_media]').wrap('<a href="adminAddMedia.html" rel="#overlay"></a>"');
+			$('input[name=add_media]').parent('a[rel]').overlay({
+				expose: 'black',
+				left: 'center',
+				closeOnClick: false,
+				onBeforeLoad: function() { 
+	        var wrap = this.getContent().find(".overlayContent"); 
+	      	wrap.load(this.getTrigger().attr("href") + ' #addmedia');
+	      },
+	      onLoad: function() {
+		      $('#overlay #addmedia div ul li').click(function() {
+		      	$(this).children('input').attr('checked','checked');
+		      	$(this).siblings().removeClass('selected');
+		      	$(this).addClass('selected');
+		      });
+    			$('input[name=media_cancel]').replaceWith('<button type="button" class="close">Cancel</button>')
+    			$('button.close').click(function() {
+    				$('input[name=add_media]').parent('a[rel]').overlay().close();
+    			});
 	      }
       });
-		}
-		
+      // end add media overlay
+		}// end “if jQuery overlay”
+		// begin “add author” functionality
+		$('input[name=add_author]').replaceWith('<a class="add_remove add_author">+&nbsp;Add author</a>');
+		var removeAuthor = ' <a class="add_remove remove_author">-&nbsp;Remove author</a>';
+		$('a.add_author').click(function() {
+			$(this).parent().before('<div><input name="author[]" type="text" />' + removeAuthor + '</div>');
+				if($('input[name=author\[\]]').length == 2) {
+					$('input[name=author\[\]]:first').after(removeAuthor);
+				}
+			$('a.remove_author').click(function() {
+				$(this).parent().remove();
+				if($('input[name=author\[\]]').length == 1) {
+					$('a.remove_author').remove();
+				}
+			});
+		});
+		// end “add author” functionality
+		// begin “add link” functionality
+		$('input[name=add_link]').replaceWith('<a class="add_remove add_link">+&nbsp;Add link</a>');
+		var removeLink = ' <a class="add_remove remove_link">-&nbsp;Remove link</a>';
+		$('a.add_link').click(function() {
+			$('#ce_link').parent().after('<div><input name="link[]" type="text" />' + removeLink + '</div>');
+				if($('input[name=link\[\]]').length == 2) {
+					$('input[name=link\[\]]:first').after(removeLink);
+				}
+			$('a.remove_link').click(function() {
+				$(this).parent().remove();
+				if($('input[name=link\[\]]').length == 1) {
+					$('a.remove_link').remove();
+				}
+			});
+		});
+		// end “add link” functionality
 		// new functions should be added here
 	});
 }
