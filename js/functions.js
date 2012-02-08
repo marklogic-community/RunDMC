4e9e2aea6055e849af1123ac5de2c868af7da0ad
Eric Bloch <eric.bloch@gmail.com>
2012-02-07 19:49:52 -0800

    Add a utility

--- js/functions.js
+++ js/functions.js
@@ -395,3 +395,15 @@ if(typeof jQuery != 'undefined') {
 
 	});
 };
+
+function getParameterByName(name)
+{
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.search);
+  if(results == null)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
