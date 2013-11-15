dbd27932a75f932da1852161df73ec7d00a58d6c
Eric Bloch <eric.bloch@gmail.com>
2013-11-14 21:01:54 -0500

    fix vidwrap

--- js/functions.js
+++ js/functions.js
@@ -697,7 +697,7 @@ if(typeof jQuery != 'undefined') {
         $('.yearpicker').val(d);
         
 
-            $("#vidwrap").click(function(e) {
+            $(".vidwrap").click(function(e) {
                 var rel = $(this).attr('rel');
                 $(this).children().replaceWith('<iframe width="460" height="259" src="http://www.youtube.com/embed/'+ rel+'?autoplay=1&rel=0&vq=hd720" frameborder="0" allowfullscreen=""></iframe>');
             });
