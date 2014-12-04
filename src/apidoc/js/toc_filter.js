ee27ad32ddb77cbee1a7ae25d11757e4c9174c2c
Michael Blakeley <mike@blakeley.com>
2014-12-04 12:57:46 -0800

    #348 idle timeout for TOC filter

--- src/apidoc/js/toc_filter.js
+++ src/apidoc/js/toc_filter.js
@@ -138,28 +138,38 @@ function toc_init() {
     });
 
     // Set up the filter
+    var filterDelay = 750;
     $("#config-filter").keyup(function(e) {
-        var currentFilterText = $(this).val();
-        LOGGER.debug("config-filter current=", currentFilterText);
+        // clear any existing idle timer
+        clearTimeout($.data(this, 'timer'));
+        var filter = $(this);
+        var currentFilterText = filter.val();
+        LOGGER.debug("config-filter not idle",
+                     new Date(), currentFilterText);
+        // delay any work until idle
+        $.data(this, 'timer', setTimeout(function() {
+            LOGGER.debug("config-filter idle",
+                         new Date(), currentFilterText);
 
-        // TODO what about a confirmation alert?
-        if (e.which == 13) // Enter key pressed
-            window.location = "/do-do-search?api=1"
-            + "&v=" + $("input[name = 'v']").val()
-            + "&q=" + $(this).val();
+            // TODO what about a confirmation alert?
+            if (e.which == 13) // Enter key pressed
+                window.location = "/do-do-search?api=1"
+                + "&v=" + $("input[name = 'v']").val()
+                + "&q=" + currentFilterText;
 
-        var closeButton = $("#config-filter-close-button");
-        if ($(this).val() === "") closeButton.hide();
-        else closeButton.show();
+            var closeButton = $("#config-filter-close-button");
+            if (currentFilterText === "") closeButton.hide();
+            else closeButton.show();
 
-        setTimeout(function() {
-            if (previousFilterText !== currentFilterText) {
-                //LOGGER.debug("toc filter event", currentFilterText);
-                previousFilterText = currentFilterText;
-                // The current TOC tree root should be visible.
-                filterConfigDetails(currentFilterText,
-                                    ".apidoc_tree:visible"); } },
-                   350);
+            setTimeout(function() {
+                if (previousFilterText !== currentFilterText) {
+                    //LOGGER.debug("toc filter event", currentFilterText);
+                    previousFilterText = currentFilterText;
+                    // The current TOC tree root should be visible.
+                    filterConfigDetails(currentFilterText,
+                                        ".apidoc_tree:visible"); } },
+                       350); },
+                                         filterDelay));
     });
 
     $("#config-filter-close-button").click(function() {
