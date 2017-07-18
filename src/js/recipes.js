c044a9c05780502057fb0d98d3bca18afe2969c0
Dave Cassel <dmcassel@gmail.com>
2017-07-18 16:36:51 -0400

    Added tags facet

--- src/js/recipes.js
+++ src/js/recipes.js
@@ -92,13 +92,24 @@ Vue.component('recipes', {
       start: 0,
       end: 0,
       recipes: [],
+      tags: [],
       searchCriteria: {
         tags: []
       }
     };
   },
   template:
-    `<div>
+    `<div class="row">
+
+      <div class="sidebar col-md-3">
+        <facet v-bind:label="'Tags'" v-bind:constraint="'tags'"
+          v-bind:values="tags"
+          v-on:searchCriteriaEvent="updateSearchCriteria"></facet>
+      </div>
+
+      <div class="col-md-9">
+        <h1>Recipes</h1>
+
       <div class="row">
         <div class="col-md-8">
           A recipe provides a reusable solution to a common problem. While a
@@ -140,14 +151,13 @@ Vue.component('recipes', {
       <pagination
         v-bind:pages="pages" v-bind:current="current"
         v-on:setPage="setPage"></pagination>
-
+      </div>
      </div>`,
   methods: {
     setPage: function(page) {
       this.current = page;
     },
     updateResults: function(results) {
-      console.log('updateResults');
       this.total = results.total;
       this.pages = results.pages;
       this.start = results.start;
@@ -158,11 +168,22 @@ Vue.component('recipes', {
         recipe.problem = decodeURIComponent(recipe.problem);
         return recipe;
       });
+      this.tags = results.tags;
     },
     updateSearchCriteria: function(criteria) {
       this.current = 1;
       if (criteria.tag) {
         this.searchCriteria.tags.push(criteria.tag);
+      }
+      if (criteria.facet !== undefined) {
+        if (criteria.facet.name === 'tags') {
+          if (criteria.facet.action === 'add') {
+            this.searchCriteria.tags.push(criteria.facet.value);
+          } else {
+            this.searchCriteria.tags =
+              this.searchCriteria.tags.filter(tag => tag !== criteria.facet.value);
+          }
+        }
       }
       if (criteria.text !== undefined) {
         this.$refs.search.runSearch();
@@ -197,6 +218,64 @@ Vue.component('recipe', {
   methods: {
     onTagClick: function(event) {
       this.$emit('searchCriteriaEvent', { tag: event.currentTarget.textContent });
+    }
+  }
+});
+
+Vue.component('facet', {
+  props: ['label', 'constraint', 'values'],
+  data: function() {
+    return {
+      selected: []
+    };
+  },
+  computed: {
+    unselected: function() {
+      let vm = this;
+      return this.values.filter(val =>
+        vm.selected.indexOf(val.name) === -1
+      );
+    }
+  },
+  template:
+    `<section class="widget">
+      <h1>{{label}}</h1>
+      <div>
+        <ul class="tags">
+          <li class="label label-primary" v-for="tag in selected">{{ tag }}
+            <i class="glyphicon glyphicon-remove" v-on:click="removeTag(tag)"></i>
+          </li>
+        </ul>
+        <ul>
+          <li v-for="value in unselected" v-on:click="facetClick">
+            <a href="#">
+              <span class="value">{{ value.name }}</span> (<span class="count">{{ value.count }}</span>)
+            </a>
+          </li>
+        </ul>
+      </div>
+     </section>`,
+  methods: {
+    facetClick: function(event) {
+      let value = event.currentTarget.querySelector('.value').textContent;
+      this.selected.push(value);
+      this.$emit('searchCriteriaEvent', {
+        facet: {
+          action: 'add',
+          name: this.constraint,
+          value: value
+        }
+      });
+    },
+    removeTag: function(tag) {
+      this.selected = this.selected.filter(i => i !== tag);
+      this.$emit('searchCriteriaEvent', {
+        facet: {
+          action: 'remove',
+          name: this.constraint,
+          value: tag
+        }
+      });
     }
   }
 });
